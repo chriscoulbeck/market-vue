c59ce570ce33822424047da25c49d8e61b2ba152
Riley Mahuika <riley.mahuika@gmail.com>
2020-09-17 11:22:23 +1200

    Landing Page Test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/"></router-link> |
+      <router-link to="/about"></router-link>
     </div>
     <router-view />
   </div>
@@ -18,7 +18,10 @@
 }
 
 #nav {
-  padding: 30px;
+  margin: 0;
+  height:60px;
+  width: 100%;
+  background-color: #333;
 
   a {
     font-weight: bold;
